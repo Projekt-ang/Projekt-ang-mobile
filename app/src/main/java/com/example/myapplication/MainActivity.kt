117ac0febcddc39304a59a62abfd391386be2d46
Lukasz <lukaszr500@gmail.com>
2019-10-20 00:33:18 +0200

    fixes

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,15 +1,24 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
+
+        var loginButton : Button = findViewById(R.id.loginButton)
+        val demoButton : Button = findViewById(R.id.demoButton)
+
+        loginButton.setOnClickListener {
+            println("test")
+        }
     }
 }
