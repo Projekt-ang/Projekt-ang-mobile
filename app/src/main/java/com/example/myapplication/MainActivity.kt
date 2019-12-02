9b15dc455cd8b6905b4fa9c835b4f20d724eaeb4
kkbielski <kkbielski@protonmail.com>
2019-12-02 21:31:15 +0100

    - improving SentenceActivity functionality, incl. putting words in right order functionality and basic validate of it - changes to .xml files - improving program logic, e.g. creating reusable components and functions, replacement of outdated functions

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+
+private val ADD_TASK_REQUEST = 1
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+
+        var loginButton : Button = findViewById(R.id.loginButton)
+        val demoButton : Button = findViewById(R.id.demoButton)
+
+        loginButton.setOnClickListener {
+            println("test")
+        }
+
+        demoButton.setOnClickListener {
+            val intent = Intent(this, ExerciseSelectionActivity::class.java)
+            startActivityForResult(intent, ADD_TASK_REQUEST)
+        }
+    }
+}
