31ca26d8304013a6a7e93e655525d9f5ba6c1c79
kkbielski <kkbielski@protonmail.com>
2020-01-22 03:13:52 +0100

    - Almost final changes to SentenceActivity algorithm

--- app/src/main/java/com/example/myapplication/SentenceActivity.kt
+++ app/src/main/java/com/example/myapplication/SentenceActivity.kt
@@ -3,7 +3,6 @@ package com.example.myapplication
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.Gravity
-import android.view.View
 import android.widget.EditText
 import android.widget.LinearLayout
 import android.widget.TextView
@@ -11,41 +10,107 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.content.ContextCompat
+import com.example.myapplication.apiclient.model.Sentence
+import com.example.myapplication.apiclient.service.Services
 import com.nex3z.flowlayout.FlowLayout
 import kotlinx.android.synthetic.main.activity_sentence.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import java.util.*
 
 
 
 class SentenceActivity : AppCompatActivity() {
 
-    private var pressCounter = 0
-    private val maxPressCounter = 5
-    private var keys = arrayOf("oranges ", "Kamil ", "much", "very ", "likes ")
 
-    private val answer = "Kamil likes oranges very much"
+
+
+
+    private var pressCounter = 0
+    private val maxPressCounter = 10
+    var polish = "Kamil bardzo lubi pomarańcze"
+
+    var keysData = "Kamil likes oranges very much"
+
+
+//    var sentence = (0..keysData.size).random()
+//    var new = keysData[sentence]
+
+    fun mainSplit(args: Array<String>) {
+        var str = keysData
+        var delimeter = " "
+
+        var parts = str.split(delimeter)
+    }
+
+    var new = keysData.split(" ").toTypedArray()
+
+    var keys = shuffleArray(new)
+
+    //    private val answer = "Kamil likes oranges very much"
     private lateinit var sentencesActivityHeader: TextView
     private lateinit var yourWords: TextView
+
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sentence)
 
-        
+//        var sentence : Sentence? = null
+//        if (intent.hasExtra("sentence")) {
+//            sentence = intent.extras!!.getParcelable("sentence")
+//            if (sentence != null) {
+//                polish = sentence!!.polishSentence.toString()
+//                keysData = sentence!!.englishSentence.toString()
+//            }
+//        }
 
-        keys = shuffleArray(keys)
+
+        var sentences : Sentence?
+        if (intent.hasExtra("sentences")) {
+            sentences = intent.extras!!.getParcelable("sentences")!!
+            if (sentences != null) {
+                polish = sentences!!.polishSentence.toString()
+                keysData = sentences!!.englishSentence.toString()
+            }else{
+                polish = "--"
+                keysData = "--"
+            }
+        } else {
+            val call: Call<Sentence> = Services.EXERCISE_SERVICE.getSentence(1)
+            call.enqueue(object : Callback<Sentence> {
+                override fun onResponse(call: Call<Sentence>, response: Response<Sentence>) {
+                    if (response.code() == 200) {
+                        sentences = response.body()!!
+                        if (sentences != null) {
+                            polish = sentences!!.polishSentence.toString()
+                            keysData = sentences!!.englishSentence.toString()
+                        }else{
+                            polish = "--"
+                            keysData = "--"
+                        }
+                    }
+                }
+                override fun onFailure(call: Call<Sentence>, t: Throwable) {
+                    println("-- Network error occured, code: " + t.toString())
+                }
+            })
+        }
+
 
         for (key in keys) {
             addView(
-                LayoutParent, key,
+                layoutParent, key,
                 findViewById(R.id.editText)
             )
 
         }
     }
 
-    private fun shuffleArray(ar: Array<String>): Array<String> {
+    fun shuffleArray(ar: Array<String>): Array<String> {
         val rnd = Random()
         for (i in ar.size - 1 downTo 1) {
             val index = rnd.nextInt(i + 1)
@@ -56,7 +121,7 @@ class SentenceActivity : AppCompatActivity() {
         return ar
     }
 
-    private fun addView(viewParent: FlowLayout, text: String, editText: EditText) {
+    fun addView(viewParent: FlowLayout, text: String, editText: EditText) {
         val flowLayoutParams = LinearLayout.LayoutParams(
             LinearLayout.LayoutParams.WRAP_CONTENT,
             LinearLayout.LayoutParams.WRAP_CONTENT
@@ -76,7 +141,6 @@ class SentenceActivity : AppCompatActivity() {
         textView.textSize = 24f
 
         sentencesActivityHeader = findViewById(R.id.sentencesActivityHeader)
-
         yourWords = findViewById(R.id.yourWords)
 
         textView.setOnClickListener {
@@ -84,21 +148,20 @@ class SentenceActivity : AppCompatActivity() {
                 if (pressCounter == 0)
                     editText.setText("")
 
-                editText.setText(editText.text.toString() + text)
+                editText.setText(editText.text.toString() + text.plus(" "))
+
                 // miejsce na animacje
                 textView.animate().alpha(0f).duration = 300
                 pressCounter++
 
-                if (pressCounter == maxPressCounter)
+                if (pressCounter == new.size) {
                     doValidate()
-
-
+                }
             }
         }
-
         viewParent.addView(textView)
-
     }
+
 
     /*
     private fun reverseView(viewParent: FlowLayout, questions: String) {
@@ -149,18 +212,22 @@ class SentenceActivity : AppCompatActivity() {
 
 
 
+
     @SuppressLint("WrongConstant")
-    private fun doValidate() {
+    fun doValidate() {
         pressCounter = 0
         val editText = findViewById<EditText>(R.id.editText)
-        val flowLayout = layoutSecond
+        val flowLayout = layoutParent
 
+//        var keysData2 = keysData.trim()
 
-        if (editText.text.toString() == answer) {
+        var keysData2 = keysData.replace("\\s".toRegex(), "")
+
+        if (editText.text.toString() == keysData2) {
             editText.setText("")
             val toast = Toast.makeText(this, "Dobrze!", 1000)
             toast.show()
-            moveSentence.visibility = View.VISIBLE
+
         } else {
             editText.setText("")
             val toast = Toast.makeText(this, "Żle!", 1000)
